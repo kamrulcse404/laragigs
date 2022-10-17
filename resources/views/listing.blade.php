10d1aa04e6941bf7bd73a72e5ee6e06b6c745cf0
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-18 04:50:45 +0600

    blade components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,10 +1,9 @@
 @extends('layout')
 
 @section('content')
-
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i>Back</a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <x-card>
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{ asset('images/no-image.png') }}" alt="" />
 
@@ -33,7 +32,7 @@
                         Job Description
                     </h3>
                     <div class="text-lg space-y-6">
-                       {{ $listing->description }}
+                        {{ $listing->description }}
 
                         <a href="mailto:{{ $listing->email }}"
                             class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i
@@ -47,6 +46,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </x-card>
     </div>
 @endsection
