81b6a713c6fe6dc1d4b2980979af7dae68b4c972
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-18 02:04:06 +0600

    eloquent model

---
+++ data.php
@@ -0,0 +1,22 @@
+<?php 
+
+return [
+    [
+        'title' => 'Laravel Serior Developer',
+        'tags' => 'laravel javascript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston, MA',
+        'email' => 'acme@gmail.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque est magnam vitae sit iure nisi molestiae numquam eaque dignissimos mollitia?'
+    ],
+    [
+        'title' => 'Full-Stack Engineer',
+        'tags' => 'php laravel react mysql',
+        'company' => 'Enosis',
+        'location' => 'Banani, Dhaka',
+        'email' => 'enosis@gmail.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque est magnam vitae sit iure nisi molestiae numquam eaque dignissimos mollitia?'
+    ]
+];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(5)->create();
+        \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'title' => 'Laravel Serior Developer',
+            'tags' => 'laravel javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'acme@gmail.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque est magnam vitae sit iure nisi molestiae numquam eaque dignissimos mollitia?'
+        ]);
+
+        Listing::create([
+            'title' => 'Full-Stack Engineer',
+            'tags' => 'php laravel react mysql',
+            'company' => 'Enosis',
+            'location' => 'Banani, Dhaka',
+            'email' => 'enosis@gmail.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque est magnam vitae sit iure nisi molestiae numquam eaque dignissimos mollitia?'
+        ]);
     }
 }
