6dba5ecd3f59141cffd6b09e1a80772018a6aa41
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-17 23:01:45 +0600

    route & basic view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore doloremque ratione voluptatem, id est porro.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore doloremque ratione voluptatem, id est porro.'
+            ]
+        ]
+    ]);
 });
