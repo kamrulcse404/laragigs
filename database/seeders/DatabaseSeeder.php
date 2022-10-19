dcfb813b03081aed55233948d0931e3f710b8ac6
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-20 03:23:04 +0600

    relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
 
-        Listing::factory(5)->create();
+        $user = User::factory()->create([
+            'name' => 'Rabbi',
+            'email' => 'rabbi@gmail.com'
+        ]);
+
+        Listing::factory(5)->create([
+            'user_id' => $user->id
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
