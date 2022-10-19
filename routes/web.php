aec2ad9ffd4a9d9df7232a57bb506f892c5fae6c
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-20 00:06:42 +0600

    delete listing

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,8 @@ Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 // Update Listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
+// delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 // single listing 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
